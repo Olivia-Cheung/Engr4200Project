c28610c2e8507f48ddcc449c59ca70e11b20fe80
Olivia-Cheung <ocheun2@lsu.edu>
2024-04-16 13:30:33 -0500

    line detection maybe

--- auto_vehicle/auto_vehicle/vision.py
+++ auto_vehicle/auto_vehicle/vision.py
@@ -21,6 +21,56 @@ class Vision(Node):
 
         self.webcam_subscription = self.create_subscription(Image, 'auto_vehicle/webcam', self.webcam_callback, 10)
 
+    def canny(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        canny = cv2.Canny(blur, 50, 150)
+        return canny
+    
+    def region_of_interest(self, canny):
+        height = canny.shape[0]
+        width = canny.shape[1]
+        mask = np.zeros_like(canny)
+        triangle = np.array([
+            [(0, height), (246, 234), (560, height)],
+        ], np.int32)
+        cv2.fillPoly(mask, triangle, 255)
+        masked_image = cv2.bitwise_and(canny, mask)
+        return masked_image
+    
+    def display_lines(self, img, lines):
+        line_image = np.zeros_like(img)
+        if lines is not None:
+            for line in lines:
+                for x1, y1, x2, y2 in line:
+                    cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+        return line_image
+    
+    def average_slope_intercept(self, image, lines):
+        left_fit = []
+        right_fit = []
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                fit = np.polyfit((x1, x2), (y1, y2), 1)
+                slope, intercept = fit
+                if slope < 0:  # left lane
+                    left_fit.append((slope, intercept))
+                else:  # right lane
+                    right_fit.append((slope, intercept))
+        if not left_fit or not right_fit:
+            return None
+        left_line = self.make_points(image, np.average(left_fit, axis=0))
+        right_line = self.make_points(image, np.average(right_fit, axis=0))
+        return [left_line, right_line]
+
+    def make_points(self, image, line):
+        slope, intercept = line
+        y1 = int(image.shape[0])
+        y2 = int(y1 * 7 / 10)
+        x1 = int((y1 - intercept) / slope)
+        x2 = int((y2 - intercept) / slope)
+        return [(x1, y1, x2, y2)]
+    
     def webcam_callback(self, msg):
         self.get_logger().info('Received webcam frame')
 
